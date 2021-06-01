6162b3adfe45ed916cfc42a64442c3496c137c52
Alexandr Sosna <50953271+AlexandrSosna@users.noreply.github.com>
2021-06-01 21:04:11 +0300

    Зроблена 10 лабораторна робота

---
+++ lab10/prj/Sosna_task/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <math.h>
+#include <clocale>
+#include "MyDevInfo.h"
+#include <fstream>
+#include <ctime>
+#include <windows.h>
+
+using namespace std;
+
+int main(){
+    SetConsoleCP(::GetACP());
+    SetConsoleOutputCP(::GetACP());
+    char dess;
+    string text = "text.txt";
+    ofstream nw;
+    nw.open(text,ios::out);
+    nw<<"(Введіть текст)";
+    nw.close();
+    cout<<"Для початку правильної роботи застосунку потрібно у створеному текстовому файлі(text.txt)\nу одній папці з застосунком записати одну із перших восьмих рядків вірша Василя Симоненка \"Вклонися їй\"\n "<<endl;
+    string out;
+    cout<<"Вкажіть назву вихідного файла (з .txt):\n>>";
+    cin>>out;
+    cout<<"\nКоли все готово натисніть \'y\'(Yes). \nАбо просто закрийте застосунок\n>>";
+    cin>>dess;
+    if(dess=='y'||dess=='Y'||dess=='н'||dess=='Н'){
+        filefindstring(text,out);
+        filesize(text);
+        lab10task3(out);
+    }else{
+        cout<<"Вихід з програми";
+         return 0;
+    }
+
+    system("pause");
+}
---
+++ lab10/prj/TestDriver/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <windows.h>
+#include "MyDevInfo.h"
+
+
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(::GetACP());
+    SetConsoleOutputCP(::GetACP());
+    int i=0;
+    string in = "text.txt";
+    string out = "out.txt";
+
+    cout<<"\tTask 10.1"<<endl;
+    for(;i<3;i++){
+        int res = filefindstring(in,out);
+        if(res==1){
+            cout<<"\tpassed"<<endl;
+        }else{
+            cout<<"failed"<<endl;
+        }
+    }
+
+    cout<<"\tTask 10.2"<<endl;
+    for(int j=0;j<3;j++){
+        int res2 = filesize(in);
+        if(res2==1){
+            cout<<"\tpassed"<<endl;
+        }else{
+            cout<<"failed"<<endl;
+        }
+    }
+
+    system("pause");
+
+    return 0;
+}
